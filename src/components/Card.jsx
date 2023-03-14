0ceed3bfbf0ae547eb40ecb8558a6d6c56b5cc2f
Nassim Bourebia <b.nassim01@gmail.com>
2023-03-14 02:10:52 +0100

    add Banner

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,11 @@ import React from "react";
 
 function Card () {
 
-    return (<div className="Card">
+    return (<div className="cardbox">
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
+        <div className="card"></div>
         
     </div>)
 }
