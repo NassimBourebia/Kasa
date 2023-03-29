d2764c7fa39e3ed45b7c7455f106a75f729ef235
Nassim Bourebia <b.nassim01@gmail.com>
2023-03-29 16:26:53 +0200

    data recovery modification

--- src/pages/Accommodation.jsx
+++ src/pages/Accommodation.jsx
@@ -1,15 +1,30 @@
+import { useEffect, useState } from "react";
 import Information from "../components/Information";
 import Main from "../components/Main";
-import data from "../__mocks__/logements.json";
 import { useParams } from "react-router-dom";
 import Carousel from "../components/Carousel";
 import Dropdown from "../components/Dropdown";
 
 function Accommodation() {
   const params = useParams();
-  const logement = data.find((item) => item.id === params.id);
+  const [ logement, setLogement ] = useState(null);
 
-  return (
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('/data/logements.json');
+        if (!response.ok) throw new Error((await response.json()).message);
+        const data = await response.json();
+        
+        const logement = data.find(item => item.id === params.id);
+        setLogement(logement);
+      } catch(error) {
+        console.error(error);
+      }
+    })();
+  }, [ params ]);
+
+  return logement && (
     <Main className="Accommodation">
       <Carousel pictures={logement.pictures} />
       <Information data={logement} />
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,25 @@
+import { useEffect, useState } from "react";
 import Banner from "../components/Banner";
 import Main from "../components/Main";
 import Card from "../components/Card";
-import data from "../__mocks__/logements.json";
 
 function Home() {
-  return (
+  const [ data, setData ] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const response = await fetch('/data/logements.json');
+        if (!response.ok) throw new Error((await response.json()).message);
+
+        setData(await response.json());
+      } catch(error) {
+        console.error(error);
+      }
+    })();
+  }, []);
+
+  return data && (
     <Main className="Home">
       <Banner text="Chez vous, partout et ailleurs" />
       <ul className="cardbox">
